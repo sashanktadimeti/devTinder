224e036e6680c59363288075f57e13f30e891abb
sashanktadimeti <sashanktadimeti4@gmail.com>
2025-05-17 17:42:07 +0530

    fouthrbarnch

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const { connectDb } = require("./config/database");
 const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({origin: 'http://localhost:5173', credentials: true}))
 const {authRouter} = require("./routes/auth")
 const {profileRouter} = require("./routes/profile")
 const {requestRouter} = require("./routes/requests")
